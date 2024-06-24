19ac78d8e08dccba062e3d1ccfac74e2541a9112
matexore <matexore1@gmail.com>
2024-06-24 18:13:33 -0300

    Ya funcionan las orientaciones, tanto al crearse como para moverse

---
+++ eliminar_oca.asm
@@ -0,0 +1,33 @@
+global eliminar_oca
+extern buscar_indice_de_oca
+
+section .text
+
+; FUNCION PRINCIPAL
+; (rdi, rsi, rdx, rcx)
+; (vector_de_ocas: rdi, tope_ocas: rsi, auxiliar_fila: dl, auxiliar_columna: cl)  
+; post: si la oca existe reduce el tope en 2 y reacomoda el vector para que la oca buscada
+;       este fuera del tope y devuelve 1 en rax, si la oca no existe devuelve -1 en rax.
+eliminar_oca:
+    call    buscar_indice_de_oca
+    cmp     rax, -1
+    je      no_se_puede_eliminar_oca
+    sub     byte[rsi], 2 ; le quito una oca al vector (con el tope)
+    mov     rbx, rax
+    ; actualizo el vector para sacar la oca que quiero eliminar
+    loop_eliminar_oca:
+        mov     al, [rsi]
+        cmp     rbx, [rsi]
+        jge     terminar_loop_eliminar_oca
+        mov     dl, [rdi + rbx + 2] ; agarro la proxima fila
+        mov     [rdi + rbx], dl ; la pongo en la fila del actual
+        mov     cl, [rdi + rbx + 3] ; agarro la proxima columna
+        mov     [rdi + rbx + 1], dl ; la pongo en la columna del actual
+        add     rbx, 2
+        jmp     loop_eliminar_oca
+    terminar_loop_eliminar_oca:
+        mov     rax, 1
+        ret
+    no_se_puede_eliminar_oca:
+        mov     rax, -1
+        ret
---
+++ inicializar_ocas.asm
@@ -0,0 +1,155 @@
+global inicializar_ocas
+
+%define ARRIBA 'W'
+%define IZQUIERDA 'A'
+%define ABAJO 'S'
+%define DERECHA 'D'
+%define MAX_FILAS 7
+%define MAX_COLUMNAS 7
+
+section .bss
+    orientacion             resb 1
+
+section .text
+
+; Asumo que el vector de ocas es una variable? los movimientos_validos tambien? (por ahora los recibo por parametro).
+; orientacion es: W, A, X o D. La orientacion default es W.
+; (rdi, rsi, rdx, rcx)
+; (vector_ocas: rdi, movimientos_validos: rsi, tope_ocas: rdx, orientacion: cl)
+; post: inicializa las ocas desde arriba hacia abajo (filas superiores) y de izquierda a derecha. 
+;       Si no me pasan una orientacion entonces la default es ARRIBA
+inicializar_ocas:
+    mov     [orientacion], cl
+    call    inicializar_movimientos_validos
+    mov     byte[rdx], 0
+    mov     rbx, 1          ; rbx va a ser i = 1
+
+    primer_loop:
+        mov     rcx, 1          ; rcx va a ser j = 1
+        cmp     rbx, MAX_FILAS  ; mientras rbx sea menor igual a 7 continua
+        jg      cerrar_primer_loop
+        segundo_loop:
+            cmp     rcx, MAX_COLUMNAS    ; mientras rcx sea menor igual a 7 continua
+            jg      cerrar_segundo_loop
+            jmp     comprobar_condiciones_de_inicializacion
+        avanzar_segundo_loop:
+            inc     rcx
+            jmp     segundo_loop
+        cerrar_segundo_loop:
+            inc     rbx
+            jmp     primer_loop
+    cerrar_primer_loop:
+        ret ; en rax no devuelvo nada
+
+    inicializar_movimientos_validos:
+        mov     byte[rsi],  IZQUIERDA
+        mov     byte[rsi + 1],  ABAJO
+        mov     byte[rsi + 2],  DERECHA
+        mov     byte[rsi + 3],  ARRIBA
+        call    sacar_movimiento_invalido
+        ret
+
+    comprobar_condiciones_de_inicializacion:
+        cmp rbx, 3      ; si la oca esta en la fila 3 la agrego
+        je agregar_a_vector_de_ocas
+
+        cmp rcx, 2
+        jle segunda_condicion_de_inicializacion
+        cmp rcx, 6
+        jge segunda_condicion_de_inicializacion
+
+        cmp rbx, 1      ; si la oca esta entre la columna 2 y 6 (sin incluir) y en la fila 1 la agrego
+        je agregar_a_vector_de_ocas
+        cmp rbx, 2      ; si la oca esta entre la columna 2 y 6 (sin incluir) y en la fila 2 la agrego
+        je agregar_a_vector_de_ocas
+
+    segunda_condicion_de_inicializacion:
+        cmp rcx, 1      ; si la oca esta en la columna 1 compruebo la siguiente condicion
+        je tercera_condicion_de_inicializacion
+        cmp rcx, 7      ; si la oca esta en la columna 7 compruebo la siguiente condicion
+        jne no_cumple_ninguna_condicion
+
+    tercera_condicion_de_inicializacion:
+        cmp rbx, 4      ; si la oca esta en la fila 4 la agrego
+        je agregar_a_vector_de_ocas
+        cmp rbx, 5      ; si la oca esta en la fila 5 la agrego
+        je agregar_a_vector_de_ocas
+
+    no_cumple_ninguna_condicion:
+        jmp     avanzar_segundo_loop    ; la oca no se puede agregar, entonces avanzo a la siguiente posicion
+
+    agregar_a_vector_de_ocas:
+        mov     [rdi], bl       ;le agrego la fila a la oca
+        mov     [rdi + 1], cl   ;le agrego la columna a la oca
+        call    reacomodar_en_base_a_orientacion
+        add     rdi, 2          ;avanzo el puntero a la siguiente oca
+        add     byte[rdx], 2
+        jmp     avanzar_segundo_loop
+
+
+    reacomodar_en_base_a_orientacion:
+        cmp     byte[orientacion], ABAJO
+        je      reacomodar_abajo
+        cmp     byte[orientacion], IZQUIERDA
+        je      reacomodar_izquierda
+        cmp     byte[orientacion], DERECHA
+        je      reacomodar_derecha
+        ret
+
+    reacomodar_abajo:
+        call    espejo
+        ret
+
+    reacomodar_izquierda:
+        call    rotar
+        ret
+
+    reacomodar_derecha:
+        call    rotar
+        call    espejo
+        ret
+
+    ; pos: doy vuelta las posiciones de la oca, cambio fila por columna y viceversa
+    rotar:
+        mov     [rdi], cl
+        mov     [rdi + 1], bl
+        ret
+
+    ; pos: a las posiciones de la oca les asigno el punto que esta al otro lado de donde se encuentran en relacion al punto (4,4)
+    ; Ej: punto (2,5) va a parar al (6, 3)
+    espejo:
+        mov     al, 4 ; le asigno al registro la fila central
+        sub     al, [rdi] ; al registro le resto la fila asignada originalmente a la oca para sacar la "distancia" a la fila central
+        add     al, 4 ; al punto central le sumo esta "distancia"
+        mov     [rdi], al ; le asigno esta nueva fila a la oca
+        mov     al, 4 ; le asigno al registro la columna central
+        sub     al, [rdi + 1] ; al registro le resto la columna asignada originalmente a la oca para sacar la "distancia" a la columna central
+        add     al, 4 ; al punto central le sumo esta "distancia"
+        mov     [rdi + 1], al ; le asigno esta nueva columna a la oca
+        ret
+
+    sacar_movimiento_invalido:
+        mov     al, [orientacion]
+        cmp     [rsi], al
+        je      sacar_movimiento_izquierda
+        cmp     [rsi + 1], al
+        je      sacar_movimiento_abajo
+        cmp     [rsi + 2], al
+        je      sacar_movimiento_derecha
+        jmp     sacar_movimiento_arriba
+
+    sacar_movimiento_izquierda:
+        mov     byte[rsi], -1
+        ret
+
+    sacar_movimiento_abajo:
+        mov     byte[rsi + 1], -1
+        ret
+
+    sacar_movimiento_derecha:
+        mov     byte[rsi + 2], -1
+        ret
+
+    sacar_movimiento_arriba:
+        mov     byte[rsi + 3], -1
+        ret
